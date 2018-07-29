d17c5a5c0ba0dfa2d6275af668b8f635bcedc55d
albertmolodec <albert.abdu@gmail.com>
2018-07-30 01:33:53 +0500

    Fix filter bug when text is not in table

--- js/scripts.js
+++ js/scripts.js
@@ -28,43 +28,84 @@ var popularPaymentMethods = getPopularPaymentMethods();
 drawPaymentsDoughnutChart('paymentsDoughnutChart');
 drawPaymentsLineChart('paymentsLineChart');
 
+
 filterableText.addEventListener('input', function() {
-    filterableRows = getSuitableRows(columnSelector.value, filterableText.value);
-    updateTable(filterableRows);
+    updateTable();
 });
 
 columnSelector.addEventListener('change', function() {
     filterableText.value = '';
-    updateTable(filterableRows, false);
+    updateTable();
 })
 
 
+function generateTableData(originalData) {
+    var tableData = [];
 
-function getSuitableRows(columnName, filterableText) {
-    filterableText = filterableText.toLowerCase();
-    var suitableRows = [];
-
-    if (columnName === '#' && isNumeric(filterableText)) {
-        suitableRows.push(filterableText);
-    } 
-    else if (columnName !== '#'){
-        for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
-            if (tableData[rowNumber][columnName] === null) continue;
-
-            var isInTable = tableData[rowNumber][columnName].toLowerCase().indexOf(filterableText);
-            if (~isInTable) {
-                suitableRows.push(rowNumber);
-            }
+    for (var i = 0; i < originalData.length; i++) {
+        var originalRow = originalData[i];
+        var transactionDate = new Date(originalRow.transaction.transfer_date);
+        var row = {
+            'Country': originalRow.user.country,
+            'User ID': originalRow.user.id,
+            'Email': originalRow.user.email,
+            'Project': originalRow.transaction.project.name,
+            'Virtual item': originalRow.purchase.virtual_items,
+            'Virtual price': originalRow.purchase.virtual_currency.amount + ' ' + originalRow.purchase.virtual_currency.name,
+            'Payment price': originalRow.payment_details.payment.amount + ' ' + originalRow.payment_details.payment.currency,
+            'Payment method': originalRow.transaction.payment_method.name,
+            'Transaction ID': originalRow.transaction.id,
+            'Date': transactionDate.toLocaleDateString('ru'),
+            'Subscription': originalRow.purchase.subscription.name
         }
+        tableData.push(row);
     }
-    else {
+
+    return tableData;
+}
+
+function createTable() {
+    for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
+        addRow(tableData, rowNumber);
+    }
+}
+
+function updateTable() {
+    var suitableRows = getSuitableRows(columnSelector.value, filterableText.value);
+    
+    // If string is empty, show all rows
+    if (filterableText.value === '') {
         suitableRows = getAllRowNumbers();
     }
 
-    return suitableRows;
+    xsollaTable.tBodies[0].innerHTML = "";
+    suitableRows.forEach(rowNumber => {
+        addRow(tableData, rowNumber);
+    });
 
-    function isNumeric(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
+    function getSuitableRows(columnName, filterableText) {
+        filterableText = filterableText.toLowerCase();
+        var suitableRows = [];
+    
+        if (columnName === '#' && isNumeric(filterableText) && +filterableText > 0 && +filterableText <= tableData.length + 1) {
+            suitableRows.push(filterableText);
+        }
+        else if (columnName !== '#') {
+            for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
+                if (tableData[rowNumber][columnName] === null) continue;
+    
+                var isInTable = tableData[rowNumber][columnName].toLowerCase().indexOf(filterableText);
+                if (~isInTable) {
+                    suitableRows.push(rowNumber);
+                }
+            }
+        }
+    
+        return suitableRows;
+    
+        function isNumeric(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        }
     }
 
     function getAllRowNumbers() {
@@ -75,6 +116,23 @@ function getSuitableRows(columnName, filterableText) {
         return rowNumbers;
     }
 }
+
+function addRow(tableData, rowNumber) {
+    var row = xsollaTable.tBodies[0].insertRow();
+    var note = tableData[rowNumber];
+
+    addCell(rowNumber, tableColumns[0]);
+    for (var columnNumber = 1; columnNumber < tableColumns.length; columnNumber++) {
+        addCell(note[tableColumns[columnNumber]], tableColumns[columnNumber]);
+    }
+
+    function addCell(text, dataTh) {
+        var td = document.createElement('td');
+        td.setAttribute('data-th', dataTh)
+        td.textContent = text;
+        row.appendChild(td);
+    }
+};
 
 function sortColumn(columnNumber, type) {
     var tbody = xsollaTable.tBodies[0];
@@ -144,6 +202,49 @@ function sortColumn(columnNumber, type) {
     }
 }
 
+function fillProjectsList() {
+    var uniqueProjects = [];
+
+    for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
+        var projectName = tableData[rowNumber]['Project'];
+        if (uniqueProjects.indexOf(projectName) === -1) {
+            uniqueProjects.push(projectName);
+        }
+    }
+
+    uniqueProjects.forEach(projectName => {
+        var li = document.createElement('li');
+        li.textContent = projectName;
+        projectsList.appendChild(li);
+    });
+}
+
+function fillFilterColumns() {
+    tableColumns.forEach(columnName => {
+        var opt = document.createElement('option');
+        opt.textContent = columnName;
+        columnSelector.appendChild(opt);
+    });
+}
+
+function getPopularPaymentMethods() {
+    var paymentMethodNames = [];
+    var paymentMethodAmounts = [];
+
+    for (var transactionNumber = 0; transactionNumber < tableData.length; transactionNumber++) {
+        var paymentMethodName = tableData[transactionNumber]['Payment method'];
+        var indexInArray = paymentMethodNames.indexOf(paymentMethodName)
+        if (indexInArray === -1) {
+            paymentMethodNames.push(paymentMethodName);
+            paymentMethodAmounts.push(1);
+        } else {
+            paymentMethodAmounts[indexInArray]++;
+        }
+    }
+
+    return { names : paymentMethodNames, amounts : paymentMethodAmounts};
+}
+
 function drawPaymentsDoughnutChart(chartId) {
     var ctx = document.getElementById(chartId).getContext('2d');
     var chart = new Chart(ctx, {
@@ -191,124 +292,6 @@ function drawPaymentsLineChart(chartId) {
         }
     });    
 }
-
-
-
-
-
-
-function getPopularPaymentMethods() {
-    var paymentMethodNames = [];
-    var paymentMethodAmounts = [];
-
-    for (var transactionNumber = 0; transactionNumber < tableData.length; transactionNumber++) {
-        var paymentMethodName = tableData[transactionNumber]['Payment method'];
-        var indexInArray = paymentMethodNames.indexOf(paymentMethodName)
-        if (indexInArray === -1) {
-            paymentMethodNames.push(paymentMethodName);
-            paymentMethodAmounts.push(1);
-        } else {
-            paymentMethodAmounts[indexInArray]++;
-        }
-    }
-
-    return { names : paymentMethodNames, amounts : paymentMethodAmounts};
-}
-
-function fillProjectsList() {
-    var uniqueProjects = [];
-
-    for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
-        var projectName = tableData[rowNumber]['Project'];
-        if (uniqueProjects.indexOf(projectName) === -1) {
-            uniqueProjects.push(projectName);
-        }
-    }
-
-    uniqueProjects.forEach(projectName => {
-        var li = document.createElement('li');
-        li.textContent = projectName;
-        projectsList.appendChild(li);
-    });
-}
-
-function fillFilterColumns() {
-    tableColumns.forEach(columnName => {
-        var opt = document.createElement('option');
-        opt.textContent = columnName;
-        columnSelector.appendChild(opt);
-    });
-}
-
-function createTable() {
-    for (var rowNumber = 0; rowNumber < tableData.length; rowNumber++) {
-        addRow(tableData, rowNumber);
-    }
-}
-
-function updateTable(suitableRows) {
-    if (suitableRows.length === 0) {
-        suitableRows = getAllRowNumbers;
-    }
-
-    function getAllRowNumbers() {
-        var rowNumbers = [];
-        for (var i = 0; i < tableData.length; i++) {
-            rowNumbers.push(i);
-        }
-        return rowNumbers;
-    }
-
-    xsollaTable.tBodies[0].innerHTML = "";
-
-    suitableRows.forEach(rowNumber => {
-        addRow(tableData, rowNumber);
-    });
-}
-
-function generateTableData(originalData) {
-    var tableData = [];
-
-    for (var i = 0; i < originalData.length; i++) {
-        var originalRow = originalData[i];
-        var transactionDate = new Date(originalRow.transaction.transfer_date);
-        var row = {
-            'Country': originalRow.user.country,
-            'User ID': originalRow.user.id,
-            'Email': originalRow.user.email,
-            'Project': originalRow.transaction.project.name,
-            'Virtual item': originalRow.purchase.virtual_items,
-            'Virtual price': originalRow.purchase.virtual_currency.amount + ' ' + originalRow.purchase.virtual_currency.name,
-            'Payment price': originalRow.payment_details.payment.amount + ' ' + originalRow.payment_details.payment.currency,
-            'Payment method': originalRow.transaction.payment_method.name,
-            'Transaction ID': originalRow.transaction.id,
-            'Date': transactionDate.toLocaleDateString('ru'),
-            'Subscription': originalRow.purchase.subscription.name
-        }
-        tableData.push(row);
-    }
-
-    return tableData;
-}
-
-function addRow(tableData, rowNumber) {
-    var row = xsollaTable.tBodies[0].insertRow();
-    var note = tableData[rowNumber];
-
-    addCell(rowNumber, tableColumns[0]);
-    for (var columnNumber = 1; columnNumber < tableColumns.length; columnNumber++) {
-        addCell(note[tableColumns[columnNumber]], tableColumns[columnNumber]);
-    }
-
-    function addCell(text, dataTh) {
-        var td = document.createElement('td');
-        td.setAttribute('data-th', dataTh)
-        td.textContent = text;
-        row.appendChild(td);
-    }
-};
-
-
 
 
 function loadOriginalData() {
